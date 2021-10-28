bf7cb700c8d4f9fb1686d0d91958559fa4732aad
DestroyerDarkNess <grupo.fisica.638@gmail.com>
2021-10-28 13:34:06 -0400

    no message

---
+++ GIFtoMP4/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AllowDrop = True
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(44, Byte), Integer), CType(CType(44, Byte), Integer), CType(CType(44, Byte), Integer))
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(548, 117)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AllowDrop = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semilight", 9.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkGray
+        Me.Label1.Location = New System.Drawing.Point(144, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(244, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Drag And Drop Gif File Or Press Click here"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "GIF File |*.gif"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "MP4 File |*.mp4"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 123)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(572, 18)
+        Me.ProgressBar1.TabIndex = 1
+        Me.ProgressBar1.Visible = False
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(572, 141)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Panel1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GIF to MP4"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ GIFtoMP4/Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form1
+
+#Region " Declare "
+
+    Private PathExecutable As String = IO.Path.Combine(IO.Path.GetTempPath, "ffmpeg.exe")
+
+    Private FfmpegArgument As String = <a><![CDATA[ -i "%FileGif%" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "%FileMP4%" ]]></a>.Value
+
+
+#End Region
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Convert(OpenFileDialog1.FileName)
+    End Sub
+
+#Region " Dragger "
+
+    Private Sub Label1_DragDrop(sender As Object, e As DragEventArgs) Handles Label1.DragDrop
+        Dim FilePath As String() = e.Data.GetData(DataFormats.FileDrop)
+        If IO.Path.GetExtension(FilePath.ToList.FirstOrDefault).ToUpper = ".GIF" Then
+            Convert(FilePath.ToList.FirstOrDefault)
+        End If
+    End Sub
+
+    Private Sub Panel1_DragDrop(sender As Object, e As DragEventArgs) Handles Panel1.DragDrop
+        Dim FilePath As String() = e.Data.GetData(DataFormats.FileDrop)
+        If IO.Path.GetExtension(FilePath.ToList.FirstOrDefault).ToUpper = ".GIF" Then
+            Convert(FilePath.ToList.FirstOrDefault)
+        End If
+    End Sub
+
+    Private Sub Label1_DragEnter(sender As Object, e As DragEventArgs) Handles Label1.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub Panel1_DragEnter(sender As Object, e As DragEventArgs) Handles Panel1.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+#End Region
+
+#Region " Methods "
+
+    Public Sub Convert(ByVal GifPath As String)
+        If IO.File.Exists(GifPath) = True Then
+
+            If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+
+                Me.ProgressBar1.Value = 10
+
+                If IO.File.Exists(SaveFileDialog1.FileName) = True Then
+                    IO.File.Delete(SaveFileDialog1.FileName)
+                End If
+
+                Me.ProgressBar1.Value = 30
+
+                If CheckFfmpeg() = False Then
+                    If ExtractFFmpeg() = False Then
+                        Me.ProgressBar1.Visible = False
+                        Me.Height = 180
+                        Exit Sub
+                    End If
+                End If
+
+                Me.Height = 57
+                ProgressBar1.Visible = True
+
+                Dim ArgumentsStr As String = FfmpegArgument.Replace("%FileGif%", GifPath).Replace("%FileMP4%", SaveFileDialog1.FileName)
+
+                Dim Proc As New System.Diagnostics.Process
+
+                Me.ProgressBar1.Value = 50
+
+                Proc.StartInfo.WindowStyle = ProcessWindowStyle.Normal
+                Proc.StartInfo.CreateNoWindow = False
+                Proc.StartInfo.UseShellExecute = True
+                Proc.StartInfo.FileName = PathExecutable
+                Proc.StartInfo.Arguments = ArgumentsStr
+                Proc.Start()
+
+                Me.ProgressBar1.Value = 90
+
+                StartMonitor(Proc)
+
+            End If
+
+        End If
+    End Sub
+
+    Private Function CheckFfmpeg() As Boolean
+        Return IO.File.Exists(PathExecutable)
+    End Function
+
+    Public Function ExtractFFmpeg() As Boolean
+        Try
+            IO.File.WriteAllBytes(PathExecutable, My.Resources.ffmpeg)
+            Return IO.File.Exists(PathExecutable)
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+#End Region
+
+#Region " Process Monitor "
+
+    Public Sub StartMonitor(ByVal TargetProcess As Process)
+        Dim Asynctask As New Task(New Action(Async Sub()
+
+                                                 For i As Integer = 0 To 2
+                                                     Try
+
+                                                         If TargetProcess Is Nothing Then
+                                                             Me.BeginInvoke(Sub()
+                                                                                Me.ProgressBar1.Visible = False
+                                                                                Me.Height = 180
+                                                                                Me.ProgressBar1.Value = 0
+                                                                            End Sub)
+                                                             Exit Sub
+                                                         ElseIf CheckProcess(TargetProcess.Id) = False Then
+                                                             Me.BeginInvoke(Sub()
+                                                                                Me.ProgressBar1.Visible = False
+                                                                                Me.Height = 180
+                                                                                Me.ProgressBar1.Value = 0
+                                                                            End Sub)
+                                                             Exit Sub
+                                                         End If
+
+                                                     Catch ex As IO.FileNotFoundException
+                                                         Me.BeginInvoke(Sub()
+                                                                            Me.ProgressBar1.Visible = False
+                                                                            Me.Height = 180
+                                                                            Me.ProgressBar1.Value = 0
+                                                                        End Sub)
+                                                         Exit Sub
+                                                     End Try
+                                                     System.Threading.Thread.Sleep(1000)
+                                                     i -= 1
+                                                 Next
+
+                                             End Sub), TaskCreationOptions.PreferFairness)
+        Asynctask.Start()
+    End Sub
+
+    Private Function CheckProcess(ByVal ID As Integer) As Boolean
+        Try
+            Dim Proc As Process = Process.GetProcessById(ID)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+#End Region
+
+End Class
